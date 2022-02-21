e8bd3e10ff1100ce755df46d018b12f141cfdbc9
David Pires <developer.david@outlook.com>
2022-02-21 09:36:38 -0300

    revising fix to set a counter

--- pages/contador.jsx
+++ pages/contador.jsx
@@ -1,19 +1,39 @@
 import { useState } from 'react';
 
-export default function () {
+export default function contador() {
   const [numero, setNumero] = useState(0)
-  function increment() {
-    setNumero(numero++)
-  }
   function decrement() {
     setNumero(numero--)
+  }
+  function increment() {
+    setNumero(numero++)
   }
   return (
     <div>
       <h1>Contador</h1>
-      <div>Valores : {numero}</div>
+      <div>Valores: {numero}</div>
       <button onClick={decrement}>-</button>
       <button onClick={increment}>+</button>
     </div>
   );
 }
+
+// import { useState } from 'react';
+
+// export default function () {
+//   const [numero, setNumero] = useState(0)
+//   function increment() {
+//     setNumero(numero++)
+//   }
+//   function decrement() {
+//     setNumero(numero--)
+//   }
+//   return (
+//     <div>
+//       <h1>Contador</h1>
+//       <div>Valores : {numero}</div>
+//       <button onClick={decrement}>-</button>
+//       <button onClick={increment}>+</button>
+//     </div>
+//   );
+// }
