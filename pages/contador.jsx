6e6130321f0b8db28a686637618ff22774c6e436
David Pires <developer.david@outlook.com>
2022-02-21 00:16:40 -0300

    countador

--- pages/contador.jsx
+++ pages/contador.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+
 export default function () {
+  const [numero, setNumero] = useState(0)
+  function increment() {
+    setNumero(numero++)
+  }
+  function decrement() {
+    setNumero(numero--)
+  }
   return (
     <div>
       <h1>Contador</h1>
-      <div>Valores:</div>
-      <button>-</button>
-      <button>+</button>
+      <div>Valores : {numero}</div>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
